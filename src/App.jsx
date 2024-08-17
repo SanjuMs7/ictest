e7a99f858c3a1459878623894fa5b6bb42345554
SanjuMs <sanjumlp777@gmail.com>
2024-08-17 11:13:07 +0530

    sticky-page-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Navbar from './navbar/navbar'
 import Dashboard from './dashboard/dashboard'
+import MainPage from './mainPage/mainPage'
 function App() {
   return (
     <>
       <Navbar/>
       <Dashboard/>
+      <MainPage/>
     </>
   )
 }
---
+++ src/mainPage/mainPage.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './mainPage.css'
+import ieeeINNER from '../assets/ieee-inner-shell.png'
+import ieeeOUTER from '../assets/ieee-outer-shell.png'
+import college from '../assets/collegeImage.jpeg'
+export default function MainPage() {
+  return (
+    <div className='main-parent-div'>
+      <div className="ieee-background-image-div">
+            <img src={ieeeINNER} alt="" />
+            <img src={ieeeOUTER} alt="" className='animating-image'/>
+      </div>
+      <div className="sticky-outer-layer-blur">
+            <h1>ieee ictest<br/>Model engineering college</h1>
+            <div className="college-video-preview">
+                  <img src={college} alt="" />
+            </div>
+      </div>
+    </div>
+  )
+}
