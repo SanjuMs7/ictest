298f0f9f072cc85171a4eb9c6dfa5daafabe0a4a
n1ved <nivedrsalini@gmail.com>
2024-08-22 20:00:58 +0530

    Add Page Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Schedule from "./schedule/schedule.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />}/>
+              <Route path="/schedules" element={<Schedule />}/>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
